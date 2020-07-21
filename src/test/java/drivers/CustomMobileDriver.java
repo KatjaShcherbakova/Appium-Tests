fe86b012a42e89a38ebb38740067e607263d6dfc
Katja <ekaterinas.scher@gmail.com>
2020-07-21 13:15:09 +0200

    build.gradle updated

--- src/test/java/drivers/CustomMobileDriver.java
+++ src/test/java/drivers/CustomMobileDriver.java
@@ -28,10 +28,6 @@ public class CustomMobileDriver implements WebDriverProvider {
         capabilities.setCapability("name", "Tests - " + platform + " - " + buildNumber);
         capabilities.setCapability("autoGrantPermissions", "true");
 
-        capabilities.setCapability("device", "Google Pixel 3");
-        capabilities.setCapability("os_version", "9.0");
-        capabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
-
         return capabilities;
     }
 
